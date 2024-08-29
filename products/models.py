31cb309f5fda70e65d1cd065bb28897513cb43ce
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-08-29 13:52:05 +0000

    feat: add the Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField()
+    base_price = models.DecimalField(decimal_places=2,
+                                     validators=[MinValueValidator(0.00)])
