8245a6c2924735a0d6f36b15094863c6bdb5af0a
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-08-29 14:08:43 +0000

    chore: create + connect db and migrate product model

--- products/models.py
+++ products/models.py
@@ -7,5 +7,6 @@ class Product(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
     description = models.TextField()
-    base_price = models.DecimalField(decimal_places=2,
+    base_price = models.DecimalField(max_digits=7,
+                                     decimal_places=2,
                                      validators=[MinValueValidator(0.00)])
