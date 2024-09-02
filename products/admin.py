7b641be6414386c83400477f63084a10805296fb
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-09-02 12:52:48 +0000

    feat: create nested admin inline structure for options in product

--- products/admin.py
+++ products/admin.py
@@ -1,23 +1,32 @@
+import nested_admin
 from django.contrib import admin
-from .models import Product, Component, ProductComponent
+from .models import Product, Option, OptionValue, Component
 
 
-# Register your models here.
-class ProductComponentInline(admin.TabularInline):
-    model = ProductComponent
-    extra = 1
+# Define inline classes to use in the admin pages
+class OptionValueInline(nested_admin.NestedTabularInline):
+    model = OptionValue
+    extra = 0
 
 
+class OptionInline(nested_admin.NestedStackedInline):
+    model = Option
+    inlines = [OptionValueInline]
+    extra = 0
+
+
+# Define the ProductAdmin page with OptionInline to add Options in same view
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(nested_admin.NestedModelAdmin):
     list_display = ('name', 'slug', 'description', 'base_price', 'created_on',
                     'updated_on')
-    inlines = [ProductComponentInline,]
+    inlines = [OptionInline,]
     search_fields = ['name', 'description']
     list_filter = ('created_on',)
     prepopulated_fields = {'slug': ('name',)}
 
 
+# Define the ComponentAdmin page to add components
 @admin.register(Component)
 class ComponentAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'description', 'unit_cost',
