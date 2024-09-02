7ef758f7906e893c38959e6d5983df4fb2d2deee
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-09-02 08:28:45 +0000

    feat: create component model class + register to admin

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Component
 
 
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'description', 'base_price')
+    list_display = ('name', 'slug', 'description', 'base_price', 'created_on',
+                    'updated_on')
     search_fields = ['name', 'description']
+    list_filter = ('created_on',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Component)
+class ComponentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'description', 'unit_cost',
+                    'measurement_unit', 'supplier_source', 'created_on',
+                    'updated_on')
+    search_fields = ['name', 'description', 'supplier_source']
+    list_filter = ('supplier_source', 'created_on',)
     prepopulated_fields = {'slug': ('name',)}
--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 
-# Create your models here.
+# Create Product model class as the main parent model
 class Product(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
@@ -10,3 +10,28 @@ class Product(models.Model):
     base_price = models.DecimalField(max_digits=7,
                                      decimal_places=2,
                                      validators=[MinValueValidator(0.00)])
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+# Create a Component model class as related class to Product model
+class Component(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+    unit_cost = models.DecimalField(max_digits=7,
+                                    decimal_places=2,
+                                    validators=[MinValueValidator(0.00)])
+    # create a units mapping to use as choices for measurement_unit
+    UNITS = {'g': 'gram',
+             'kg': 'kilogram',
+             'l': 'litre',
+             'pc': 'piece'}
+    measurement_unit = models.CharField(
+        max_length=2,
+        choices=UNITS,
+        default='pc'
+    )
+    supplier_source = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
