0a5728e5bf852e2b7de41ebb118ace7746656dfa
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-09-02 09:58:16 +0000

    feat: link Product with Component via ProductComponent class

--- products/admin.py
+++ products/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Product, Component
+from .models import Product, Component, ProductComponent
 
 
 # Register your models here.
+class ProductComponentInline(admin.TabularInline):
+    model = ProductComponent
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'description', 'base_price', 'created_on',
                     'updated_on')
+    inlines = [ProductComponentInline,]
     search_fields = ['name', 'description']
     list_filter = ('created_on',)
     prepopulated_fields = {'slug': ('name',)}
--- products/models.py
+++ products/models.py
@@ -2,24 +2,6 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 
-# Create Product model class as the main parent model
-class Product(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=50, unique=True)
-    description = models.TextField(blank=True, null=True)
-    base_price = models.DecimalField(max_digits=7,
-                                     decimal_places=2,
-                                     validators=[MinValueValidator(0.00)])
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ["name"]
-
-    def __str__(self):
-        return f"{self.name} | Base price at €{self.base_price}"
-
-
 # Create a Component model class as related class to Product model
 class Component(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -47,3 +29,35 @@ class Component(models.Model):
 
     def __str__(self):
         return f"{self.name} | €{self.unit_cost} per {self.measurement_unit}"
+
+
+# Create Product model class as the main parent model
+class Product(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+    base_price = models.DecimalField(max_digits=7,
+                                     decimal_places=2,
+                                     validators=[MinValueValidator(0.00)])
+    components = models.ManyToManyField(Component, through='ProductComponent')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} | Base price at €{self.base_price}"
+
+
+# Create a ProductComponent intermediary model to link the Product with its
+# componets and to be able to define a quantity of each component that make up
+# the product
+class ProductComponent(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    component = models.ForeignKey(Component, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return (f"{self.product.name} - {self.component.name} "
+                f"x ({self.quantity})")
