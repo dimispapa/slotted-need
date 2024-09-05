17ca6998884a37ffd5cc5e96b1d428bbf1e8c251
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-09-05 16:36:24 +0000

    feat: register orders models in admin page

--- orders/admin.py
+++ orders/admin.py
@@ -1,19 +1,24 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Client, Order, OrderItem
 
 
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    extra = 1
-    filter_horizontal = ['option_values', 'finishes']
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('client_name', 'client_phone',
+                    'client_email', 'created_on')
 
 
-# Register your models here.
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('customer_name', 'customer_phone',
-                    'customer_email', 'created_on',
+
+    class OrderItemInline(admin.TabularInline):
+        model = OrderItem
+        extra = 1
+        # filter_horizontal = ['option_values', 'finishes']
+
+    list_display = ('client', 'discount',
+                    'order_status', 'created_on',
                     'updated_on')
-    inlines = [OrderItemInline,]
-    search_fields = ['customer_name', 'customer_email']
+    inlines = [OrderItemInline, ]
+    # search_fields = ['client_name', 'client_email']
     list_filter = ('created_on',)
