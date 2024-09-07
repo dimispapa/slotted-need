89e7742ba264c14276bf6b7f860af09ad9986cf8
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-09-07 08:50:00 +0000

    refactor: add trailing slash to url pattern

--- orders/urls.py
+++ orders/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     # get_products API
     path('api/get_products/', views.get_products, name='get_products'),
     # get_product_options API, with product_id as a path parameter
-    path('api/get_product_options/<int:product_id>', views.get_product_options,
-         name='get_product_options'),
+    path('api/get_product_options/<int:product_id>/',
+         views.get_product_options, name='get_product_options'),
     # search_clients API, with client_name as a query parameter set in the view
     path('api/search_clients/', views.search_clients, name='search_clients')
 ]
