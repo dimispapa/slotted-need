a1cb7eb880f1c61643de2e2891024ae1da405c7f
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-09-09 14:33:58 +0000

    feat: add price, discount + value to orders & item

--- orders/models.py
+++ orders/models.py
@@ -17,9 +17,10 @@ class Order(models.Model):
     client = models.ForeignKey(Client, related_name='orders',
                                on_delete=models.SET_NULL,
                                blank=True, null=True)
-    discount = models.DecimalField(max_digits=7, decimal_places=2,
-                                   validators=[MinValueValidator(0.00)],
-                                   default=0.00)
+    discount = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0, editable=False)
+    order_value = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0, editable=False)
     # create a status mapping to use as choices for order_status
     STATUS = {1: 'Not Started',
               2: 'In Progress',
@@ -32,8 +33,21 @@ class Order(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    def calculate_totals(self):
+        # Sum the discount and order_value from all OrderItems
+        items = self.items.all()
+        self.discount = sum(item.discount * item.quantity for item in items)
+        self.order_value = sum(item.item_value for item in items)
+        self.save()
+
+    def save(self, *args, **kwargs):
+        # Call the method to calculate totals before saving
+        self.calculate_totals()
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"Order #{self.id} by {self.client.client_name}"
+        return (f"Order #{self.id} by {self.client.client_name} "
+                f"- Order value: €{self.order_value}")
 
 
 class OrderItem(models.Model):
@@ -41,10 +55,22 @@ class OrderItem(models.Model):
                               on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='order_items',
                                 on_delete=models.CASCADE)
+    base_price = models.DecimalField(max_digits=10, decimal_places=2,
+                                     validators=[MinValueValidator(0.00)])
+    discount = models.DecimalField(max_digits=10, decimal_places=2,
+                                   validators=[MinValueValidator(0.00)],
+                                   default=0.00, blank=True, null=True)
+    item_value = models.DecimalField(max_digits=10, decimal_places=2,
+                                     editable=False)
     quantity = models.PositiveIntegerField(default=1)
     option_values = models.ManyToManyField(OptionValue,
                                            related_name='order_items')
     finish_options = models.ManyToManyField(FinishOption, blank=True)
 
+    def save(self, *args, **kwargs):
+        # Automatically calculate item_value before saving
+        self.item_value = (self.base_price - self.discount) * self.quantity
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"{self.quantity}x {self.product.name}"
+        return f"{self.quantity}x {self.product.name}: €{self.item_value}"
