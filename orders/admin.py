db1daa910401150072a206c50790fac7f67ff3fe
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-09-17 09:49:54 +0000

    feat: add order no + value to order admin

--- orders/admin.py
+++ orders/admin.py
@@ -16,9 +16,14 @@ class OrderAdmin(admin.ModelAdmin):
         extra = 0
         filter_horizontal = ['option_values', 'finish_options']
 
-    list_display = ('client', 'discount',
+    list_display = ('order_number', 'client', 'order_value',
                     'order_status', 'created_on',
                     'updated_on')
+
+    # render the order number from the primary key
+    def order_number(self, obj):
+        return f"Order#{obj.pk}"  # Format the order number as "Order#pk"
+
     inlines = [OrderItemInline, ]
     search_fields = ['client_name', 'client_email']
     list_filter = ('created_on',)
