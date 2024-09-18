58493b4ab71c29aaa43984aa8eef82b606678aee
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-09-18 16:16:22 +0000

    feat: imrpove order admin layout with nested

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,4 @@
+import nested_admin
 from django.contrib import admin
 from .models import Client, Order, OrderItem
 
@@ -9,21 +10,20 @@ class ClientAdmin(admin.ModelAdmin):
 
 
 @admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
+class OrderAdmin(nested_admin.NestedModelAdmin):
 
-    class OrderItemInline(admin.TabularInline):
+    class OrderItemInline(nested_admin.NestedStackedInline):
+        # filter_horizontal = ['option_values']
         model = OrderItem
         extra = 0
-        filter_horizontal = ['option_values', 'finish_options']
 
     list_display = ('order_number', 'client', 'order_value',
                     'order_status', 'created_on',
                     'updated_on')
+    inlines = [OrderItemInline]
+    search_fields = ['client__client_name', 'client__client_email']
+    list_filter = ['created_on']
 
     # render the order number from the primary key
     def order_number(self, obj):
         return f"Order#{obj.pk}"  # Format the order number as "Order#pk"
-
-    inlines = [OrderItemInline, ]
-    search_fields = ['client_name', 'client_email']
-    list_filter = ('created_on',)
