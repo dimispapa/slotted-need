c5cc03bcf462647d009caf6427cb40013017aacc
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-09-20 15:37:41 +0000

    feat: add func to handle paid status styles

--- static/js/orders.js
+++ static/js/orders.js
@@ -5,31 +5,47 @@ document.addEventListener('DOMContentLoaded', function () {
     // Define function that updates the item_status dropdown bg colour 
     function updateSelectStyle() {
 
-        // Global constants definition
+        // Get select elements
         let itemStatusSelects = document.querySelectorAll('.item-status-select');
+        let paidStatusSelects = document.querySelectorAll('.paid-status-select');
 
-        // loop through dropdowns
+        // loop through item status dropdowns
         itemStatusSelects.forEach(select => {
             // remove existing style classes
-            select.classList.remove('bg-primary' ,'bg-pending', 'bg-success', 'bg-secondary');
+            select.classList.remove('bg-primary', 'bg-pending', 'bg-success', 'bg-secondary');
             // Not Started
             if (select.value == 1) {
-              select.classList.add('bg-primary');
-            // In Progress
+                select.classList.add('bg-primary');
+                // In Progress
             } else if (select.value == 2) {
                 select.classList.add('bg-pending');
-            // Made
+                // Made
             } else if (select.value == 3) {
-              select.classList.add('bg-success');
-            // Delivered
+                select.classList.add('bg-success');
+                // Delivered
             } else if (select.value == 4) {
-              select.classList.add('bg-secondary');
+                select.classList.add('bg-secondary');
             }
-
             // Add listener to update styling on change
             select.addEventListener('change', updateSelectStyle);
         });
-      };
+
+        // loop through dropdowns
+        paidStatusSelects.forEach(select => {
+            // remove existing style classes
+            select.classList.remove('bg-notice', 'bg-secondary', 'text-decoration-line-through', 'text-light');
+            // Not Paid
+            if (select.value == 1) {
+                select.classList.add('bg-notice');
+                // Fully Paid
+            } else if (select.value == 2) {
+                select.classList.add('bg-secondary', 'text-decoration-line-through', 'text-light');
+            }
+            // Add listener to update styling on change
+            select.addEventListener('change', updateSelectStyle);
+        });
+
+    };
 
     // ************** SECTION B: EVENT LISTENERS & HANDLERS *****************************************************************
 
