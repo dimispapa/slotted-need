e82a838dd6ba829ed44ece0643fcc90435f86eef
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-09-26 15:33:02 +0000

    refactor: move displayMessages func to utils.js

--- static/js/item_tracker.js
+++ static/js/item_tracker.js
@@ -1,3 +1,5 @@
+import { displayMessages } from './utils.js';
+
 $(document).ready(function () {
 
     // Global constants definition
@@ -13,6 +15,10 @@ $(document).ready(function () {
             }
         }
     });
+
+    if (data.messages && data.messages.length > 0) {
+        displayMessages(data.messages);
+    }
 
     // Initialize DataTable with AJAX source and server-side processing
     let table = $('#orderitem-table').DataTable({
@@ -240,4 +246,48 @@ $(document).ready(function () {
         applyFilters();
     }, 300));
 
+
+    // Function to display messages
+    function displayMessages(messages) {
+        // Clear existing messages
+        messageContainer.innerHTML = '';
+
+        messages.forEach(msg => {
+            // Create a div for each message
+            const messageDiv = document.createElement('div');
+            messageDiv.classList.add('alert', 'alert-dismissible', 'fade', 'show');
+            // Determine the alert class based on message level
+            switch (msg.level_tag) {
+                case 'debug':
+                case 'info':
+                    messageDiv.classList.add('alert-info');
+                    break;
+                case 'success':
+                    messageDiv.classList.add('alert-success');
+                    break;
+                case 'warning':
+                    messageDiv.classList.add('alert-warning');
+                    break;
+                case 'error':
+                    messageDiv.classList.add('alert-danger');
+                    break;
+                default:
+                    messageDiv.classList.add('alert-secondary');
+            }
+
+            messageDiv.setAttribute('role', 'alert');
+            messageDiv.innerText = msg.message;
+
+            // add a close button
+            const closeBtn = document.createElement('button');
+            closeBtn.type = 'button';
+            closeBtn.classList.add('btn-close');
+            closeBtn.setAttribute('data-bs-dismiss', 'alert');
+            closeBtn.setAttribute('aria-label', 'Close');
+            messageDiv.appendChild(closeBtn);
+
+            // Append the message to the container
+            messageContainer.appendChild(messageDiv);
+        });
+    }
 })
--- static/js/order_tracker.js
+++ static/js/order_tracker.js
@@ -1,3 +1,5 @@
+import { displayMessages } from './utils.js'
+
 document.addEventListener('DOMContentLoaded', function () {
 
     // Constant definitions
@@ -5,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const deleteModal = new bootstrap.Modal(document.getElementById('DeleteOrderConfirmationModal'));
     const confirmDeleteBtn = document.getElementById("confirm-delete-order-btn");
     const csrftoken = document.querySelector("meta[name='csrf-token']").content;
-    const messageContainer = document.getElementById('msg-container');
 
     // ************** SECTION A: FUNCTION DEFINITIONS ********************************************************************
 
@@ -114,50 +115,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 }]);
             });
     };
-
-    // Function to display messages
-    function displayMessages(messages) {
-        // Clear existing messages
-        messageContainer.innerHTML = '';
-
-        messages.forEach(msg => {
-            // Create a div for each message
-            const messageDiv = document.createElement('div');
-            messageDiv.classList.add('alert', 'alert-dismissible', 'fade', 'show');
-            // Determine the alert class based on message level
-            switch (msg.level_tag) {
-                case 'debug':
-                case 'info':
-                    messageDiv.classList.add('alert-info');
-                    break;
-                case 'success':
-                    messageDiv.classList.add('alert-success');
-                    break;
-                case 'warning':
-                    messageDiv.classList.add('alert-warning');
-                    break;
-                case 'error':
-                    messageDiv.classList.add('alert-danger');
-                    break;
-                default:
-                    messageDiv.classList.add('alert-secondary');
-            }
-
-            messageDiv.setAttribute('role', 'alert');
-            messageDiv.innerText = msg.message;
-
-            // add a close button
-            const closeBtn = document.createElement('button');
-            closeBtn.type = 'button';
-            closeBtn.classList.add('btn-close');
-            closeBtn.setAttribute('data-bs-dismiss', 'alert');
-            closeBtn.setAttribute('aria-label', 'Close');
-            messageDiv.appendChild(closeBtn);
-
-            // Append the message to the container
-            messageContainer.appendChild(messageDiv);
-        });
-    }
 
     // ************** SECTION B: EVENT LISTENERS & HANDLERS *****************************************************************
 
