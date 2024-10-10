07c0f41fec396988928ccd1b55a2c667b99ced8c
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-10-10 15:40:35 +0000

    fix: dlt old migration errors despite db being ok
    the order of execution caused errors for the test db when recreated

--- slotted_need/settings.py
+++ slotted_need/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -27,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  'localhost',]
@@ -112,6 +113,10 @@ WSGI_APPLICATION = 'slotted_need.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+# Testing database
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
