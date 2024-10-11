733b705b7bdda0214fa8a187e32aef3e994a5fae
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-10-11 08:19:19 +0000

    test: add tests for non-admin users + refactor

--- slotted_need/test_home.py
+++ slotted_need/test_home.py
@@ -16,7 +16,9 @@ class TestHomeView(TestCase):
                           password='testpass')
 
     def test_home_renders_correctly(self):
-        """Test for the home view rendering correctly"""
+        """
+        Test for the home view rendering correctly for an admin
+        """
         response = self.client.get(reverse('home'))
 
         # check that the response is 200 OK
@@ -44,8 +46,24 @@ class TestHomeView(TestCase):
         self.assertContains(response, b'canvas')
         self.assertContains(response, b'orderitem-home-table')
 
+    def test_non_admin_cannot_access(self):
+        """
+        Test that a non-admin user cannot access
+        """
+        # Create a client with non-admin user
+        self.user = baker.make(User, is_staff=False, is_superuser=False)
+        self.user.set_password('testpass')
+        self.user.save()
+        self.client.login(username=self.user.username, password='testpass')
 
-class ProductRevenueAPITest(TestCase):
+        response = self.client.get(reverse('home'))
+
+        # check for non 200 status code
+        self.assertNotEqual(response.status_code, 200,
+                            msg='Anauthorised access allowed')
+
+
+class TestProductRevenueDataAPI(TestCase):
 
     def setUp(self):
         # Create a superuser / admin
@@ -57,30 +75,54 @@ class ProductRevenueAPITest(TestCase):
                           password='testpass')
 
         # Create test data
-        self.product = baker.make('products.Product')
-        self.product2 = baker.make('products.Product')
+        # self.product = baker.make('products.Product')
+        # self.product2 = baker.make('products.Product')
         self.customer = baker.make('orders.Client')
-
         self.order = baker.make('orders.Order',
                                 client=self.customer)
         self.order_item = baker.make('orders.OrderItem',
                                      order=self.order,
-                                     product=self.product)
+                                     #  product=self.product
+                                     )
         self.order_item2 = baker.make('orders.OrderItem',
                                       order=self.order,
-                                      product=self.product2)
+                                      #   product=self.product2
+                                      )
 
-    def test_product_revenue_data(self):
+    def test_product_revenue_data_admin(self):
         """
         Test if the product revenue data response from the API
         matches the product and order item instances of the order
         """
+        # make an API request
         response = self.client.get(reverse('product_revenue_data'))
+        # check for 200 status code
         self.assertEqual(response.status_code, 200)
+        # parse json data
         data = response.json()
-        expected_labels = [self.product.name, self.product2.name]
-        expected_values = [float(self.order_item.item_value),
-                           float(self.order_item2.item_value)]
 
-        self.assertEqual(data['labels'], expected_labels)
-        self.assertEqual(data['values'], expected_values)
+        # Assert if the correct labels and values are in the response
+        # that chartjs charts use to render data
+        expected_labels = sorted([item.product.name
+                                  for item in self.order.items.all()])
+        expected_values = sorted([float(item.item_value)
+                                  for item in self.order.items.all()])
+        self.assertListEqual(expected_labels, sorted(data['labels']))
+        self.assertListEqual(expected_values, sorted(data['values']))
+
+    def test_product_revenue_data_non_admin(self):
+        """
+        Test a request from a non-admin user to ensure
+        that it gets rejected
+        """
+        # Create a client with non-admin user
+        self.user = baker.make(User, is_staff=False, is_superuser=False)
+        self.user.set_password('testpass')
+        self.user.save()
+        self.client.login(username=self.user.username, password='testpass')
+
+        # make an API request
+        response = self.client.get(reverse('product_revenue_data'))
+        # check for non 200 status code
+        self.assertNotEqual(response.status_code, 200,
+                            msg='Anauthorised access allowed')
