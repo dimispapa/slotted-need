1d155606db6da685f8c595c7686504156e55c71f
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-11-19 12:40:00 +0000

    test: add test for create order view rendering

--- orders/test_order_form.py
+++ orders/test_order_form.py
@@ -20,7 +20,23 @@ class TestCreateOrderView(TestCase):
 
         # Create test products and options
         self.product = baker.make(Product)
-        self.option_value = baker.make(OptionValue, option__product=self.product)
+        self.option_value = baker.make(OptionValue,
+                                       option__product=self.product)
 
         # Define the URL for creating an order
         self.create_order_url = reverse('create_order')
+
+    def test_create_order_view_renders_correctly(self):
+        """
+        Test for rendering the create order view with the correct template
+        """
+        response = self.client.get(self.create_order_url)
+        # Check that the response is 200 OK
+        self.assertEqual(response.status_code, 200, msg='Response NOT OK')
+        # Check that the correct template was used
+        self.assertTemplateUsed(response, 'orders/create_order.html')
+        # Check if form and formset are in the response context
+        self.assertIn('order_form', response.context,
+                      msg='Order form missing in context')
+        self.assertIn('order_item_formset', response.context,
+                      msg='Order item formset missing in context')
