ec07aaf7eab8982583732dcfb58fb5f723bdaede
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-11-21 15:39:48 +0000

    test: add test for invalid form data + formset

--- orders/test_order_form.py
+++ orders/test_order_form.py
@@ -66,11 +66,52 @@ class TestCreateOrderView(TestCase):
         response = self.client.post(self.create_order_url, data)
         # pass the post request to the form and formset objects
         order_form = OrderForm(data=data)
-        # check if the form has no errors
+        order_item_formset = OrderItemFormSet(data, prefix='items')
+        # check if the form and forsmet have no errors
         self.assertTrue(order_form.is_valid())
+        self.assertTrue(order_item_formset.is_valid())
         # Check if the form submission was successful
         self.assertEqual(response.status_code, 302, msg='Redirect NOT OK')
         # Check if the test client (customer) was created successfully
         self.assertTrue(Order.objects.filter(client__client_name='Test Client'
                                              ).exists(),
                         msg='Client was not created')
+
+    def test_create_order_with_invalid_data(self):
+        """
+        Test create order form submission with invalid data
+        """
+        data = {
+            'client_name': '',  # Missing client name
+            'client_phone': '1234567890',
+            'client_email': 'invalidemail',  # Invalid email format
+            'order_value': '-100.00',
+            'deposit': '20.00',
+            'items-TOTAL_FORMS': '1',
+            'items-INITIAL_FORMS': '0',
+            'items-0-product': self.product.id,
+            'items-0-quantity': '-1',  # Negative quantity
+            'items-0-base_price': '-100.00',  # Negative base price
+            'items-0-discount': '0.00',
+            'items-0-item_value': '-100.00',  # Negative item value
+        }
+
+        response = self.client.post(self.create_order_url, data)
+        # Check that the response is 200,
+        # meaning the form re-renders with errors
+        self.assertEqual(response.status_code, 200,
+                         msg='Invalid form did not re-render correctly')
+        # pass the post request to the form and formset objects
+        order_form = OrderForm(data=data)
+        order_item_formset = OrderItemFormSet(data, prefix='items')
+        # check if the form has all expected errors
+        self.assertFalse(order_form.is_valid())
+        self.assertIn('client_name', order_form.errors)
+        self.assertIn('client_email', order_form.errors)
+        # Check errors in the OrderItemFormSet
+        self.assertFalse(order_item_formset.is_valid(),
+                         msg='Order item formset should be invalid')
+        for form in order_item_formset:
+            self.assertIn('base_price', form.errors)
+            self.assertIn('item_value', form.errors)
+            self.assertIn('quantity', form.errors)
