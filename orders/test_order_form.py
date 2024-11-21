3e878d4257cf1b371a7832ef79edfcdcd9bb2540
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-11-21 13:21:55 +0000

    test: add order form data valid test

--- orders/test_order_form.py
+++ orders/test_order_form.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from model_bakery import baker
 from products.models import Product, OptionValue
+from orders.models import Order
+from orders.forms import OrderForm
 
 
 class TestCreateOrderView(TestCase):
@@ -40,3 +42,36 @@ class TestCreateOrderView(TestCase):
                       msg='Order form missing in context')
         self.assertIn('order_item_formset', response.context,
                       msg='Order item formset missing in context')
+
+    def test_create_order_with_valid_data(self):
+        """
+        Test form submission with valid data
+        """
+        data = {
+            'client_name': 'Test Client',
+            'client_phone': '1234567890',
+            'client_email': 'testclient@example.com',
+            'order_value': '220.00',
+            'deposit': '20.00',
+            'items-TOTAL_FORMS': '1',
+            'items-INITIAL_FORMS': '0',
+            'items-0-product': self.product.id,
+            'items-0-quantity': '1',
+            'items-0-base_price': '220.00',
+            'items-0-discount': '0.00',
+            'items-0-item_value': '220.00',
+            'items-0-option_1': self.option_value.id,
+        }
+
+        response = self.client.post(self.create_order_url, data)
+        # pass the post request to the form and formset objects
+        order_form = OrderForm(data=data)
+        # check if the form has no errors
+        self.assertTrue(order_form.is_valid())
+        # Check if the form submission was successful
+        self.assertEqual(response.status_code, 302, msg='Redirect NOT OK')
+        # self.assertTrue(Order.objects.filter(client__client_name='Test Client'
+        #                                      ).exists(),
+        #                 msg='Client was not created')
+        # self.assertIn(b"Order created successfully!", response.content,
+        #               msg="Success message not displayed")
