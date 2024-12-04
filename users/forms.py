277809c18da9ddb14e081cfd1effad87996826dc
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-12-04 17:03:03 +0000

    feat: add admin group of permissions to admin user

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserChangeForm, PasswordResetForm
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
@@ -46,6 +46,29 @@ class CustomUserCreationForm(forms.ModelForm):
 
         return email2
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+
+        # Save the user first
+        if commit:
+            user.save()
+
+        # Automatically add the user to Admin Group if is_staff=True
+        if user.is_staff:
+            try:
+                admin_group = Group.objects.get(name='Admin Group')
+                if not user.groups.filter(name='Admin Group').exists():
+                    user.groups.add(admin_group)
+            # raise validation error if the admin group does not exist
+            except Group.DoesNotExist:
+                raise ValidationError('Cannot set this user as Admin, '
+                                      'because the Admin permissions group '
+                                      'does not exist.'
+                                      'Please check the Admin page or contact '
+                                      'the app administrator.')
+
+        return user
+
 
 class CustomUserChangeForm(UserChangeForm):
     password = None  # Hide password field
@@ -67,6 +90,29 @@ class CustomUserChangeForm(UserChangeForm):
         if current_user and not current_user.is_staff:
             # Remove the 'is_staff' field for non-admin users
             self.fields.pop('is_staff', None)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+
+        # Save the user first
+        if commit:
+            user.save()
+
+        # Automatically add the user to Admin Group if is_staff=True
+        if user.is_staff:
+            try:
+                admin_group = Group.objects.get(name='Admin Group')
+                if not user.groups.filter(name='Admin Group').exists():
+                    user.groups.add(admin_group)
+            # raise validation error if the admin group does not exist
+            except Group.DoesNotExist:
+                raise ValidationError('Cannot set this user as Admin, '
+                                      'because the Admin permissions group '
+                                      'does not exist.'
+                                      'Please check the Admin page or contact '
+                                      'the app administrator.')
+
+        return user
 
 
 class PasswordSetupForm(PasswordResetForm):
